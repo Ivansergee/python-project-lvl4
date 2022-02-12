4132835d394efa5c88eacf2c6b34f8e530aad86f
Ivan Klimentev <klimentievis@inbox.ru>
2022-02-12 07:31:17 +0300

    working

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -44,7 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'task_manager'
+    'bootstrap4',
+    'task_manager',
 ]
 
 MIDDLEWARE = [
